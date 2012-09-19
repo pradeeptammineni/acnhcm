7c34aa4456e99390dbb4a1a3aaeff232f5abade1
Pradeep Tammineni <pradeep.tammineni@accenture.com>
2012-09-19 13:04:04 +0530

    Update javascripts/hcmapp.js

--- javascripts/hcmapp.js
+++ javascripts/hcmapp.js
@@ -222,7 +222,7 @@ function onAddUpdate() {
 	addState = $("#addState").val();
 	addCountry = $("#addCountry").val();
 	soap_envelope = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:sap-com:document:sap:soap:functions:mc-style"><soapenv:Header/><soapenv:Body><urn:AddressempChange><City>'+addCity+'</City><Coname>'+addCO+'</Coname><Country>'+addCountry+'</Country><District></District><Employeenumber>'+personID+'</Employeenumber><Lockindicator></Lockindicator><Nocommit></Nocommit><Objectid></Objectid><Postalcodecity>'+addCode+'</Postalcodecity><Recordnumber></Recordnumber><Scndaddressline>'+addLine2+'</Scndaddressline><State>'+addState+'</State><Streetandhouseno>'+addLine1+'</Streetandhouseno><Subtype>'+ADDRESS_SUBTYPE+'</Subtype><Telephonenumber></Telephonenumber><Validitybegin>'+dValBeg+'</Validitybegin><Validityend>'+dValEnd+'</Validityend></urn:AddressempChange></soapenv:Body></soapenv:Envelope>';
-	console.log(soap.envelope);
+	console.log(soap_envelope);
 	osapi.jive.connects.post({
 			'alias' : 'SAPHCM',
 			'href' : '/z_bapi_addressemp_change/801/z_bapi_addressemp_change/bind1',
