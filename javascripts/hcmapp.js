1ec50b95f14738fac02d8b6e68738ffac5a8452e
Pradeep Tammineni <pradeep.tammineni@accenture.com>
2012-10-02 02:43:36 +0530

    Update javascripts/hcmapp.js

--- javascripts/hcmapp.js
+++ javascripts/hcmapp.js
@@ -4,6 +4,7 @@ var isAddFet = 0, isBanFet = 0, isPerFet = 0, isPayFet = 0, isAction = 0, isSing
 var ADDRESS_SUBTYPE = '3';
 var BASICPAY_SUBTYPE = '0';
 var BANKDETAIL_SUBTYPE = '0';
+var PERSDOC_SUBTYPE = '3';
 var SAPEMPID_LENGTH = 8;
 
 
@@ -544,6 +545,36 @@ function onAddUpdate() {
 // On double-clicking each row, let's 
 // get the details displayed...
 $('tr.rowPerson').live('dblclick',function(){
+	personID = '';
+	personID = $(this).attr('id');
+	console.log("id: "+personID);
+	var fullName = $(this).find(".fName").html() +" "+ $(this).find(".lName").html();
+	var compCode = $(this).find(".cCode").html();
+	var orgText = $(this).find(".oText").html();
+	var jobText = $(this).find(".jText").html();
+	var posText = $(this).find(".pText").html();
+	var costCenter = $(this).find(".cCenter").html();
+	var emailID = $(this).find(".eID").html();
+	
+	$("#detailName").html(fullName);
+	$("#detailCCode").html(compCode);
+	$("#detailTeam").html(orgText);
+	$("#detailJTitle").html(jobText);
+	$("#detailPTitle").html(posText);
+	$("#detailCCenter").html(costCenter);
+	$("#detailEID").html(emailID);
+	
+	$("#displayRecord").hide();
+	$("#detailRecord").show();
+	showAddress();
+	gadgets.window.adjustHeight();
+	//console.log(personID+"--"+fullName+"--"+compCode+"--"+orgText+"--"+jobText+"--"+posText+"--"+costCenter+"--"+emailID);
+});
+
+// On double-clicking each row, let's 
+// get the details displayed...
+//$('tr.rowPerson').live('dblclick',function(){
+$('tr.rowPerson').dblclick(function(){
 	personID = '';
 	personID = $(this).attr('id');
 	console.log("id: "+personID);
